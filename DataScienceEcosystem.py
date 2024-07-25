2d27ed448053a8c8bb4f51663da08a5a59fe20da
davinay88 <108228367+davinay88@users.noreply.github.com>
2024-07-25 08:12:46 +0530

    Add files via upload
    Coursera - Tools for Data Science - Module 6 - Jupyter Notebook - Final Assignment

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ###  In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List some of the popular languages that Data Scientists use.
+# - List the commonly used libraries by Data Scientists.
+# - List a few Data Science Tools
+# - Code a simple arithmetic function to multiply then add integers
+
+# #### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Julia
+# 4. Scala
+# 5. SQL
+# 6. C++
+# 7. Java
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. ScikitLearn
+# 6. Keras
+# 7.Pytorch
+
+# #### Create a markdown cell with a table of Data Science tools
+# 
+# |Data Science Tools|
+# | ---------------- |
+# |Jupyter Lab/Notebooks|
+# |R Studio|
+# |Spyder|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+x = 200/60
+print("Hours : " ,x)
+
+
+# ## Author
+# 
+# ###### Vinay D A
+
+# In[ ]:
+
+
+
+
